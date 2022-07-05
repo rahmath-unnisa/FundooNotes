448a3d2b21411ffa46b5be138a3d88a30e2b0ace
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-07-05 20:47:32 +0530

    [Rahmath]Refactor: Computing Archive And Trash

--- fundooNotes/src/middlewares/auth.middleware.js
+++ fundooNotes/src/middlewares/auth.middleware.js
@@ -9,22 +9,26 @@ import jwt from 'jsonwebtoken';
  * @param {Object} res
  * @param {Function} next
  */
-export const userAuth = async (req, res, next) => {
+ export const userAuth = async (req, res, next) => {
+  console.log("Inside middleware---->>>",req.body);
   try {
     let bearerToken = req.header('Authorization');
-    console.log(bearerToken);
+    console.log("Bearer token---->>>>",bearerToken);
+
     if (!bearerToken)
       throw {
         code: HttpStatus.BAD_REQUEST,
-        message: 'Authorization token is required'
+        message: `Authorization token is required`
       };
     bearerToken = bearerToken.split(' ')[1];
 
-    const  user  = await jwt.verify(bearerToken, process.env.SECRETKEY);
-    console.log(user);
-    req.body.UserID= user.email;
+    const user = await jwt.verify(bearerToken, process.env.SECRET_KEY);
+    req.body.UserID = user.email;
     next();
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `UnAuthorised token`
+    });
   }
 };
