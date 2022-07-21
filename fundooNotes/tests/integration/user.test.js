35a51a840c73f2451d93a8189af7f1b142151cc0
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-07-21 08:58:37 +0530

    [Rahmath]Refactored: Testing Is Added

--- fundooNotes/tests/integration/user.test.js
+++ fundooNotes/tests/integration/user.test.js
@@ -5,39 +5,39 @@ import mongoose from 'mongoose';
 import app from '../../src/index';
 
 describe('User APIs Test', () => {
-  // before((done) => {
-  //   const clearCollections = () => {
-  //     for (const collection in mongoose.connection.collections) {
-  //       mongoose.connection.collections[collection].deleteOne(() => {});
-  //     }
-  //   };
+  before((done) => {
+    const clearCollections = () => {
+      for (const collection in mongoose.connection.collections) {
+        mongoose.connection.collections[collection].deleteOne(() => {});
+      }
+    };
 
-  //   const mongooseConnect = async () => {
-  //     await mongoose.connect(process.env.DATABASE_TEST);
-  //     clearCollections();
-  //   };
+    const mongooseConnect = async () => {
+      await mongoose.connect(process.env.DATABASE_TEST);
+      clearCollections();
+    };
 
-  //   if (mongoose.connection.readyState === 0) {
-  //     mongooseConnect();
-  //   } else {
-  //     clearCollections();
-  //   }
+    if (mongoose.connection.readyState === 0) {
+      mongooseConnect();
+    } else {
+      clearCollections();
+    }
 
-  //   done();
-  // });
+    done();
+  });
 
-  // describe('GET /users', () => {
-  //   it('should return empty array', (done) => {
-  //     request(app)
-  //       .get('/api/v1/users')
-  //       .end((err, res) => {
-  //         expect(res.statusCode).to.be.equal(200);
-  //         expect(res.body.data).to.be.an('array');
+  describe('GET /users', () => {
+    it('should return empty array', (done) => {
+      request(app)
+        .get('/api/v1/users')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.equal(200);
+          expect(res.body.data).to.be.an('array');
 
-  //         done();
-  //       });
-  //   });
-  // });
+          done();
+        });
+    });
+  });
   describe('registration', () => {
     const inputBody =  {
       "firstname" : "Saran",
